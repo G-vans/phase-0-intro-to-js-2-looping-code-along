b725f5348afccee3a9eb056fe4b0dcde3b53d3b1
G-vans <jevans.otieno@student.moringaschool.com>
2022-08-22 15:02:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const writeCards = (names) => {
+  return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+}
+console.log (writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+const countDown = (number) => {
+  for (let i = number; i >= 0; i--) {
+      console.log(i)
+  }
+}
+
+countDown(10);
+countDown(4);
